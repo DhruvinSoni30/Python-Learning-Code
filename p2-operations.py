6014076d334f5a6cffc3738e7227903ef1ed6cea
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-01 02:52:07 +0530

    Commting code

---
+++ p2-operations.py
@@ -0,0 +1,21 @@
+a = int(input("Enter the First Number:"))
+b = int(input("Enter the Second Number:"))
+c = int(input("Enter the Third Number:"))
+
+sum = a + b
+subs = a - b
+mult = a * b
+divi = a / b
+mode = a % b
+pow = a ** b
+ope = a + b * c
+ope2 = (a + b) * c
+
+print ("Addition:", sum)
+print ("Substraction:", subs)
+print ("Multiplication:", mult)
+print ("Division:", divi)
+print ("Mod:", mode)
+print ("Power:", pow)
+print ("Operation 1:", ope)
+print ("Operation 2:", ope2)
