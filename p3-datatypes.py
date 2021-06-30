4286b872d59b9fcfd2b39c31fecb55a5515eab0d
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-01 03:13:51 +0530

    Committing 3rd code

---
+++ p3-datatypes.py
@@ -0,0 +1,15 @@
+x = 5
+y = [1,2,3]
+z = ("apple", "banana", "cherry")
+a = "Dhruvin"
+b = {"name" : "John", "age" : 36}
+c = {"apple", "banana", "cherry"}
+d = True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
