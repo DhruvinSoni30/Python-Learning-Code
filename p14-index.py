c579d7992d8507ba9f2c79dc7b6dfc194c983033
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-01 06:08:23 +0530

    Adding code

---
+++ p14-index.py
@@ -0,0 +1,10 @@
+name = "Dhruvin"
+
+print(name[0])
+print(name[0:3])
+print(name[0:3:2]) #Stepover
+print(name[0:]) #Go till the end
+print(name[:4]) #Go till the 4th index
+print(name[::1])
+print(name[-1]) #Give the last character
+print(name[::-1]) #Reverse the order
