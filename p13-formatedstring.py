b3f613fefce14185ea9c73e0dcc9b8d3d96708e3
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-01 05:55:46 +0530

    Adding code

---
+++ p13-formatedstring.py
@@ -0,0 +1,7 @@
+name = 'Dhruvin'
+age = 23
+
+print(f'hi {name}. I am {age} years old')
+print('Hy {}. I am {} years old'.format('Dhruvin', '23'))
+print('Hy {1}. I am {0} years old'.format('Dhruvin', '23'))
+print('Hy {new_name}. You are {age} years old'.format(new_name='diya', age='18'))
