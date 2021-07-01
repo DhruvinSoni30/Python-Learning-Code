a4d2aed2944c1fd334e310fe8803f9964b84405b
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-02 00:50:27 +0530

    Adding code

---
+++ p15-builtinfunc.py
@@ -0,0 +1,21 @@
+print (len("Hello"))
+name = "Dhruvin"
+
+print (name[0])
+print (name[0:])
+print (name[0:len(name)])
+
+surname = 'soni'
+print (surname.upper())
+print (surname.capitalize())
+
+middlename = "KALPESH"
+print (middlename.lower())
+
+quote = "to be or not to be"
+print (quote.find("be"))
+print (quote.replace("be", "me"))
+print(quote)
+
+name2 = "DiYa"
+print (name2.swapcase())
